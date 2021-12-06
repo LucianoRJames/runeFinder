e1581cc77203e8595f52f1ca831187c6136c65dd
Luciano James <Luciano.James-CIC@ibm.com>
2021-12-06 11:38:10 +0000

    added in new function and tests
    added in the calculator function
    added a type test for the calculator function
    added in tests for a string parser function

---
+++ app.js
@@ -0,0 +1,7 @@
+const missingNumberCalculator = (equation) => {
+  return 1;
+};
+
+module.exports = {
+  missingNumberCalculator,
+};
--- test/test.js
+++ test/test.js
@@ -2,24 +2,44 @@ const assert = require("assert");
 const { expect } = require("chai");
 const app = require("../App");
 
-describe("Calculator", function () {
-  it("If the function receives a string it should return an integer", function () {
-    assert.equal(typeof app.calculator("1234"), "number");
+describe("missingNumberCalculator", function () {
+  it("Given the function receives a string it should return an integer", function () {
+    assert.equal(typeof app.missingNumberCalculator("1234"), "number");
+  });
+
+  it("Given the function receives an argument that is not of type string, it should return a type error", function () {
+    assert.throws(
+      () => {
+        app.missingNumberCalculator(123);
+      },
+      Error,
+      "Type error: arguments must be of type String"
+    );
   });
 
   it("Given the function receives an impossible equation, it should return -1", function () {
-    assert.equal(app.calculator("2-1?=5"), -1);
+    assert.equal(app.missingNumberCalculator("2-1?=5"), -1);
   });
 
   it("Given the function receives a valid addition equation, it should return the value of the ?", function () {
-    assert.equal(app.calculator("1+1=?"), 2);
+    assert.equal(app.missingNumberCalculator("1+1=?"), 2);
   });
 
   it("Given the function receives a valid subtraction equation, it should return the value of the ?", function () {
-    assert.equal(app.calculator("6-3=?"), 3);
+    assert.equal(app.missingNumberCalculator("6-3=?"), 3);
   });
 
   it("Given the function receives a valid multiplication equation, it should return the value of the ?", function () {
-    assert.equal(app.calculator("1*4=?"), 4);
+    assert.equal(app.missingNumberCalculator("1*4=?"), 4);
+  });
+});
+
+describe("stringParser", function () {
+  it("Given the function receives a string it should return an array", function () {
+    assert.equal(Array.isArray(app.stringParser("1234")), true);
+  });
+
+  it("Given the function receives an equation, it should split the string into its components (number 1, operator, number 2, equals sign, number 3) as an array", function () {
+    assert.equal(app.stringParser("1*4=?"), ["1", "*", "4", "=", "?"]);
   });
 });
