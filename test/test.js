72abaac2b99a0c6bf7b0012b3b8b09dc735c147d
Luciano James <Luciano.James-CIC@ibm.com>
2021-12-06 13:52:42 +0000

    added format tests for the argument in stringParser

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const app = require("../App");
 
 describe("missingNumberCalculator", function () {
   it("Given the function receives a string it should return an integer", function () {
-    assert.equal(typeof app.missingNumberCalculator("1234"), "number");
+    assert.equal(typeof app.missingNumberCalculator("1+1=?"), "number");
   });
 
   it("Given the function receives an argument that is not of type string, it should return a type error", function () {
@@ -13,7 +13,7 @@ describe("missingNumberCalculator", function () {
         app.missingNumberCalculator(123);
       },
       Error,
-      "Type error: arguments must be of type String"
+      "Type error: Arguments must be of type String"
     );
   });
 
@@ -36,7 +36,7 @@ describe("missingNumberCalculator", function () {
 
 describe("stringParser", function () {
   it("Given the function receives a string it should return an array", function () {
-    assert.equal(Array.isArray(app.stringParser("1234")), true);
+    assert.equal(Array.isArray(app.stringParser("1+1=?")), true);
   });
 
   it("Given the function receives an multiplication equation, it should split the string into its components (number 1, operator, number 2, equals sign, number 3) as an array", function () {
@@ -47,5 +47,34 @@ describe("stringParser", function () {
   });
   it("Given the function receives an addidion equation, it should split the string into its components (number 1, operator, number 2, equals sign, number 3) as an array", function () {
     expect(app.stringParser("1+1=?")).to.eql(["1", "+", "1", "=", "?"]);
+  });
+
+  it("Given the function receives an equation that is in the incorrect format, it should return an error", function () {
+    assert.throws(
+      () => {
+        app.stringParser("testString");
+      },
+      Error,
+      "Error: The string must be in the form of an equation"
+    );
+  });
+
+  it("Given the function receives an equation that has multiple operators, it should return an error", function () {
+    assert.throws(
+      () => {
+        app.stringParser("1+1+1=?");
+      },
+      Error,
+      "Error: The equation can only have 1 operator"
+    );
+  });
+  it("Given the function receives an equation that doesn't contain numbers or ?, it should return an error", function () {
+    assert.throws(
+      () => {
+        app.stringParser("test+string=?");
+      },
+      Error,
+      "Error: The equation must only contain numbers or ?"
+    );
   });
 });
